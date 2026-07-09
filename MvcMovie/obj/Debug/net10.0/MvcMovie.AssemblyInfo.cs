8589269d2a92b4c467111d65c7ae143348f01813
akwero-sophia <akwerosophia636@gmail.com>
2026-07-09 10:13:03 +0300

    send

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12feff79a07dd1565b7bb2dbba0872bd1dd65246")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae604acaf3c2ac6bb8550dea98a24af369ec36d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
